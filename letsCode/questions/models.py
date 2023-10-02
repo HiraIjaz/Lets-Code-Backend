3719e9381d829d95f5d1a5f2694422b20fb0c2d0
Hira <hiraijaz956@gmail.com>
2023-10-02 14:33:37 +0500

    refactored

--- letsCode/questions/models.py
+++ letsCode/questions/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 
 class Question(models.Model):
+    TYPE_CHOICES = (
+        ("mcq", "MCQ"),
+        ("coding", "Coding"),
+    )
     title = models.TextField(help_text="title or description of the question")
     # The 'data' field stores JSON data representing the question details.
     data = models.JSONField()
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100,choices=TYPE_CHOICES, default="mcq")
     isDeleted = models.BooleanField(default=False)
 
     objects = models.Manager()
